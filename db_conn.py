e407461a72d176617e8297828eb3e4d9a95f43bd
pt3Project <angelahoa188.gmail.com>
2022-09-08 19:03:45 +1000

    load csv

--- db_conn.py
+++
@@ -1,2 +0,0 @@
-# Heroku connection !
-DB_conn = 'postgresql://dzvvuadeabjykv:52e20e27a11369714177a77897b26b72f75c29f0320ee9b3114a7b21c5a4edd4@ec2-34-199-68-114.compute-1.amazonaws.com:5432/ddthf1b3u3h6eu'
